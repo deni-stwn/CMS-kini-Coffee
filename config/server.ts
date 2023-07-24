ce595247ce819ebafc399fc6204dc68d9dd73ddf
Deni setiawan <denistwn67@gmail.com>
2023-07-24 13:39:12 +0700

    update config

--- config/server.ts
+++ config/server.ts
@@ -1,14 +1,11 @@
 export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS"),
   },
-  url: env('PUBLIC_URL', 'http://localhost:1337'),
+  url: env("PUBLIC_URL", "http://localhost:1337"),
   webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
-  },
-  admin: {
-    url: env('PUBLIC_URL', 'http://localhost:1337') // Menambahkan konfigurasi URL publik untuk admin panel
+    populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
   },
 });
